764b043f95368061eb283a6da890c43b8ba34b88
dengus14 <ddgusev0318@gmail.com>
2026-03-19 08:24:43 -0500

    fix: resolve duplicate column mapping.

--- src/main/java/com/worldcupsim/demo/model/BehaviorWeights.java
+++ src/main/java/com/worldcupsim/demo/model/BehaviorWeights.java
@@ -1,5 +1,6 @@
 package com.worldcupsim.demo.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,8 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BehaviorWeights {
+    @Column(name = "behavior_shoot_tendency")
     private Double shootTendency;
+
+    @Column(name = "behavior_pass_tendency")
     private Double passTendency;
+
+    @Column(name = "behavior_aggression")
     private Double aggression;
+
+    @Column(name = "behavior_positioning")
     private Double positioning;
 }
